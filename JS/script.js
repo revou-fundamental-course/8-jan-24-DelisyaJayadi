1a924d7751bd5ac3064632bb32406a61a81b2cb0
Delisya Jayadi <delisyajayadi10@gmail.com>
2024-01-16 22:57:39 +0800

    first progress

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+function areaK(nilai) {
+    var result = parseInt(nilai) * parseInt(nilai);
+    document.getElementById("outputLuas").innerHTML = result;
+}
+
+function areaL(nilai){
+    var result = parseInt(nilai) * 4;
+    document.getElementById("outputKeliling").innerHTML = result;
+}
